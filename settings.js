87c9147df6fec18d16b081a46b809a9dc68ce0fe
DANIELenwose <enwosedaniel@gmail.com>
2025-06-14 14:08:59 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Daniel', // Your name
-  ownerNumber: '7078518049', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '9017091946', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
